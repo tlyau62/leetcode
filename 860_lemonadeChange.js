6d6a77008e46cfb6697eedf4e531890d8f66e394
Tsz Lam Yau <yaurfu@gmail.com>
2018-07-03 03:06:16 +0800

    747

--- 860_lemonadeChange.js
+++ 860_lemonadeChange.js
@@ -3,26 +3,26 @@
  * @param {number[]} bills
  * @return {boolean}
  */
-var lemonadeChange = function (bills) {
-    const money = [0, 0, 0];
+var lemonadeChange = function(bills) {
+    const money = [0,0,0];
     for (const b of bills) {
         if (b === 5) {
             money[0]++;
         } else if (b === 10) {
             money[1]++;
-
+            
             if (money[0] > 0) {
-                money[0]--;
+                money[0]--;    
             } else {
                 return false;
-            }
+            }        
         } else if (b === 20) {
             money[2]++;
-
+            
             if (money[1] > 0) {
                 money[1]--;
                 if (money[0] > 0) {
-                    money[0]--;
+                    money[0]--;    
                 } else {
                     return false;
                 }
@@ -35,6 +35,6 @@ var lemonadeChange = function (bills) {
             return false;
         }
     }
-
+    
     return true;
 };
