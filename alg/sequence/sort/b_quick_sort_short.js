d3f03b2b936552365301efdab114446a07c6b93f
tlyau62 <tlyau62@gmail.com>
2018-09-28 23:58:27 +0800

    improve base case

--- alg/sequence/sort/b_quick_sort_short.js
+++ alg/sequence/sort/b_quick_sort_short.js
@@ -2,8 +2,8 @@
 // https://stackoverflow.com/questions/7717691/why-is-the-minimalist-example-haskell-quicksort-not-a-true-quicksort
 // not in-place
 function quick_sort(A) {
-    if (A.length === 0) {
-        return [];
+    if (A.length <= 1) {
+        return A;
     }
     return quick_sort(A.filter((a) => a < A[0]))
         .concat(A.filter((a) => a === A[0]))
