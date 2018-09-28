b17b9ee004810fdd2517c392f1d3f81bf347f60e
tlyau62 <tlyau62@gmail.com>
2018-09-29 00:33:24 +0800

    added

---
+++ alg/sequence/sort/c_merge_2_sort_lists_func.js
@@ -0,0 +1,19 @@
+// functional
+function merge(A, B) {
+    if (A.length === 0 && B.length === 0) {
+        return A;
+    } else if (A.length === 0) {
+        return B;
+    } else if (B.length === 0) {
+        return A;
+    } else {
+        if (A[0] < B[0]) {
+            return [A[0]].concat(merge(A.slice(1), B));
+        } else {
+            return [B[0]].concat(merge(A, B.slice(1)));
+        }
+    }
+}
+
+console.log(merge([1, 3, 5], [2, 4, 6]));
+console.log(merge([1, 5, 9, 14, 17], [1, 6, 6, 9, 14, 15]));
