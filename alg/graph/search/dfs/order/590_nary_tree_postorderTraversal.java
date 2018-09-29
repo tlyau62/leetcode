43228344947b28ab0731efbd98d234e01cea3ae6
tlyau62 <tlyau62@gmail.com>
2018-09-29 16:18:16 +0800

    added

---
+++ alg/graph/search/dfs/order/590_nary_tree_postorderTraversal.java
@@ -0,0 +1,36 @@
+/**
+ * problem:
+ * https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+ */
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {    
+    
+    private List<Integer> res = new ArrayList<>();
+    
+    public List<Integer> postorder(Node root) {
+        
+        if (root != null) {
+            for (Node node: root.children) {
+                postorder(node);
+            }
+            res.add(root.val);   
+        }
+        
+        return res;
+    }
+    
+}
