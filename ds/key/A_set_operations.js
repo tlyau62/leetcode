9214d8628bee6b5813cd4b0699b72d30650deac8
tlyau62 <tlyau62@gmail.com>
2018-10-06 23:13:22 +0800

    add isSubset, equals

--- ds/key/A_set_operations.js
+++ ds/key/A_set_operations.js
@@ -1,20 +1,25 @@
 /**
- * set operations
+ * set operations O(n)
  * reference: http://2ality.com/2015/01/es6-set-operations.html
  */
-// O(max(n,m))
 Set.prototype.union = function (set) {
     return new Set([...this, ...set]);
 }
 
-// O(n)
 Set.prototype.intersects = function (set) {
     return new Set([...this].filter((e) => set.has(e)));
 }
 
-// O(n)
 Set.prototype.minus = function (set) {
     return new Set([...this].filter((e) => !set.has(e)));
+}
+
+Set.prototype.isSubset = function (set) {
+    return [...this].every((e) => set.has(e));
+}
+
+Set.prototype.equals = function (set) {
+    return this.isSubset(set) && set.isSubset(this);
 }
 
 /**
@@ -26,3 +31,7 @@ const set2 = new Set([2, 4, 5, 9]);
 console.log(set1.union(set2)); // [1, 2, 4, 5, 6, 8, 9]
 console.log(set1.intersects(set2)); // [4]
 console.log(set1.minus(set2)); // [1, 6, 8]
+console.log(set1.equals(set2));
+
+console.log(new Set([2, 4, 6, 8]).equals(new Set([4, 2, 8, 6])));
+console.log(new Set([2, 4, 6, 8]).equals(new Set([4, 2, 8, 7])));
