f1b8b945eecb829d512173ce6e57b895b8ad29fe
tlyau62 <tlyau62@gmail.com>
2018-10-15 23:02:01 +0800

    opt code

--- alg/sequence/sort/922_sort_by_parity_2.js
+++ alg/sequence/sort/922_sort_by_parity_2.js
@@ -5,20 +5,38 @@
  * @param {number[]} A
  * @return {number[]}
  */
-var sortArrayByParityII = function(A) {
+var sortArrayByParityII_opt = function (A) {
+    let evenIdx = 0, oddIdx = 1;
+    const res = [];
+
+    for (let i = 0; i < A.length; i++) {
+        let idx;
+        if (A[i] % 2 === 0) {
+            res[evenIdx] = A[i];
+            evenIdx += 2;
+        } else {
+            res[oddIdx] = A[i];
+            oddIdx += 2;
+        }
+    }
+
+    return res;
+};
+
+var sortArrayByParityII = function (A) {
     const oddAvail = [], evenAvail = [];
     const res = [];
-    
+
     for (let i = 0; i < A.length; i++) {
         getAvail(oddAvail, evenAvail, i).push(i);
     }
-    
+
     for (let i = 0; i < A.length; i++) {
         res[getAvail(oddAvail, evenAvail, A[i]).pop()] = A[i];
     }
-    
+
     return res;
-    
+
     function getAvail(oddAvail, evenAvail, i) {
         if (i % 2 === 0) {
             return evenAvail;
