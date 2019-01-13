412f87ea5e5ce2e0cce42a850719883df2176969
Tsz Lam Yau <yaurfu@gmail.com>
2019-01-13 15:44:35 +0800

    Add mixin

--- design/mixin.js
+++ design/mixin.js
@@ -5,7 +5,9 @@
  * ref:
  * https://en.wikipedia.org/wiki/Mixin
  * http://usejsdoc.org/tags-mixin.html
- * https://google.github.io/styleguide/jsguide.html
+ * https://javascript.info/mixins
+ * https://medium.com/google-developers/exploring-es7-decorators-76ecb65fb841
+ * https://stackoverflow.com/questions/2582289/what-is-the-difference-between-a-mixin-and-the-decorator-pattern
  * 
  * @author Tsz Lam <tlyau62@gmail.com>
  */
