a350dee26cc95d7a066759709777de562cff8ef5
Tim Yau <tyautl@nexify.com.hk>
2019-02-28 09:51:12 +0800

    Fix prove

--- alg/math/number-theory/gcd.js
+++ alg/math/number-theory/gcd.js
@@ -11,12 +11,17 @@
  * - The greatest divisor of 2 identical numbers is itself
  * 
  * Prove 2: d|a and d|b <-> d|a-b and d|b, if a > b
- * 1. d|a and d|b
- * 2.   a = dq
- *      b = dr
- * 3.     a - b = dq - dr
- *              = d(q - r)   [d|a - b]
- * 4. the common divisors of a and b are exactly the common divisors of a - b and b,
+ * 1. prove: if d|a and d|b, then d|a-b  
+ *      a = dq
+ *      b = dr  
+ *      a - b = dq - dr
+ *            = d(q - r)   [d|a - b]
+ * 2. prove: if d|a-b and d|b, then d|a and d|b
+ *      b = dq
+ *      a - b = qr
+ *      a - b + b = dq + qr
+ *              a = d(q + r) [d|a]
+ * 3. the common divisors of a and b are exactly the common divisors of a - b and b,
  *    so, in particular, they have the same greatest common divisor.
  * 
  * ref:
