3a8b11c86e26617c8515a1ed98d52fcd7a638949
Tsz Lam <tyautl@nexify.com.hk>
2021-04-15 22:59:59 +0800

    Add maybe function

---
+++ function-programming/book/functions/maybe.js
@@ -0,0 +1,12 @@
+const curry = require("./curry");
+
+// maybe :: b -> (a -> b) -> Maybe a -> b
+const maybe = curry((v, f, m) => {
+  if (m.isNothing) {
+    return v;
+  }
+
+  return f(m.$value);
+});
+
+module.exports = maybe;
